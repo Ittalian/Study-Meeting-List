d274c486a5c695fe626b6a2b3e7c7931d6e5918c
Ittalian <bothmather@au.com>
2024-08-03 05:02:17 +0900

    微修正

--- lib/view/tile_list.dart
+++ lib/view/tile_list.dart
@@ -13,13 +13,14 @@ class TileList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      padding: const EdgeInsets.only(bottom: 750),
         child: ListView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             itemCount: titleList.length,
             itemBuilder: (context, index) {
               return Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 100),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
                   child: ListTile(
                     tileColor: Colors.white,
                     title: Text(
